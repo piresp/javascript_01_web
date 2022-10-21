5d6d78a209281ac05f7e499b0bb24385f647557a
piresp <gabrielfpp47@gmail.com>
2022-10-21 11:22:36 -0300

    aula_01_js
    funcão 'tocaSom' implementada dinamicamente

--- main.js
+++ main.js
@@ -4,12 +4,13 @@ function tocaSom(idElementoAudio) {
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let i = 0;
+for (let i = 0; i < listaDeTeclas.length; i++) {
 
-while (i < listaDeTeclas.length) {
-    listaDeTeclas[i].onclick = function () {
-        tocaSom('som_tecla_pom');
+    const tecla = listaDeTeclas[i];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
     };
-
-    i = i + 1;
 }
